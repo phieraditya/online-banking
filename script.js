bd902f41ee83df5c23c37fb686617fe30631dbc8
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-01-03 16:17:03 +0700

    calculate and display balance functionality

--- script.js
+++ script.js
@@ -84,6 +84,12 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} IDR`;
+};
+calcDisplayBalance(account1.movements);
+
 const createUsernames = function (accs) {
   accs.forEach(acc => {
     acc.username = acc.owner
