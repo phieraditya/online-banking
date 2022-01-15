5fe510cdb11c033a0a65fb0fdf3c83ea6ca4f3ab
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-01-15 14:56:55 +0700

    apply timer for loan approval

--- script.js
+++ script.js
@@ -305,14 +305,16 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Add loan date
-    currentAccount.movementDates.push(new Date().toISOString());
+      // Add loan date
+      currentAccount.movementDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+    }, 5000);
   }
   // Clear input fields
   inputLoanAmount.value = null;
